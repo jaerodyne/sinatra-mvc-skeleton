8679c07c00187ed2070feedc93cac732770120c3
Jillian Somera <jsomera@gmail.com>
2016-08-18 13:50:12 -0500

    Tweak css and add seed file for dummy users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,16 +3,15 @@ get '/users/new' do
 end
 
 post '/users' do
-  p params
   @user = User.new(params['user'])
   if @user.save
+
     redirect "/users/#{@user.id}"
   else
     @errors = @user.errors.full_messages
     erb :'/users/new'
   end
 end
-
 
 get '/users/:id' do
   @user = User.find(params[:id])
